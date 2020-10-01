1d3768593b074e663d19b018baeb56ff27b75255
Heitor Hellou <heitor.hellou@outlook.com>
2020-10-01 17:47:20 -0300

    Picking a pilot

---
+++ Racing Simulator/Assets/MovingArrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingArrow : MonoBehaviour
+{
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+      transform.position = new Vector2(transform.position.x - 5f, 2f);
+      CheckBoundaries();
+    }
+
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+      transform.position = new Vector2(transform.position.x + 5f, 2f);
+      CheckBoundaries();
+    }
+  }
+
+  public void CheckBoundaries()
+  {
+    if (transform.position.x > 5f)
+    {
+      transform.position = new Vector2(5f, 2f);
+    }
+    if (transform.position.x < -5f)
+    {
+      transform.position = new Vector2(-5f, 2f);
+    }
+  }
+}
