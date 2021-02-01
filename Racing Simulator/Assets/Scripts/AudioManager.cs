9b9e228302d7615843dc8ea2f179eeedc171a8b4
Othon <othonvvalentim@gmail.com>
2021-01-31 23:18:08 -0300

    Audio Manager + Showcase

---
+++ Racing Simulator/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+  public AudioSource backMusic;
+
+  public void Start()
+  {
+    
+  }
+
+  public void PauseMusic()
+  {
+    backMusic = GameObject.Find("Music Source").GetComponent<AudioSource>();
+    backMusic.Pause();
+  }
+  public void PlayMusic()
+  {
+    backMusic = GameObject.Find("Music Source").GetComponent<AudioSource>();
+    backMusic.Play();
+  } 
+}
---
+++ Racing Simulator/Assets/Showcase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Showcase : MonoBehaviour
+{
+  public TextMeshProUGUI piloto1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    piloto1.text = World.pilots[0].Name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
